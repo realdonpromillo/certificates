ba471d2f5e4f0d37e94113bd7f3bc81b97f03bf5
realdonpromillo <61872897+realdonpromillo@users.noreply.github.com>
2023-03-19 09:54:50 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -8,4 +8,4 @@ while true; do
     echo Deploy command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn -b :5000 --access-logfile - --error-logfile - cert:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - certificates:app
