d651c056576e9772efb06ce810f6cbf9f813d16d
realdonpromillo <61872897+realdonpromillo@users.noreply.github.com>
2023-03-20 13:49:35 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECURITY_KEY_FORMS'
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                'sqlite:///' + os.path.join(basedir, 'app.db')
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
         CERTIFICATES_PER_PAGE = 10
