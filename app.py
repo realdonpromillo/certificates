de85660cddcfc1080a185818a216f09c56f26fa5
nick3053 <nick97+git@protonmail.com>
2023-03-20 11:06:36 +0100

    Maybe it works now?

--- app.py
+++ app.py
@@ -2,7 +2,3 @@ from app import create_app, db
 from app.models import User, Certificate
 
 app = create_app()
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Certificate': Certificate}
--- config.py
+++ config.py
@@ -5,9 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'erratest-du-nie'
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                'sqlite:///' + os.path.join(basedir, 'app.db')
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECURITY_KEY_FORMS'
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
         CERTIFICATES_PER_PAGE = 10
         LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
